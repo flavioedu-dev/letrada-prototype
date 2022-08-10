6643c32e6087cfa67f118cb6285fb4675c33cc5b
Flávio Eduardo <xflax00@gmail.com>
2022-08-09 21:47:34 -0300

    Mural

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from './components/Logo/Logo'
 import Video from './components/Video/Video'
 import Infografo from './components/Infografo/Infografo'
 import Equipe from './components/Equipe/Equipe'
+import Mural from './components/Mural/Mural'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Video></Video>
       <Infografo></Infografo>
       <Equipe></Equipe>
+      <Mural></Mural>
     </div>
   );
 }
