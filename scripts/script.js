d904cae06bc9e31699f39ef9fdd656bf9fc7179b
Flávio Eduardo <xflax00@gmail.com>
2023-01-04 21:25:57 -0300

    Btn Follow - Ok

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
-let btn = document.querySelector('.mobile-nav')
-let nav = document.querySelector('.nav')
+const btnNav = document.querySelector('.mobile-nav')
+const nav = document.querySelector('.nav')
+const btnIcon = document.getElementById("btn-icon")
+const btnFollow = document.querySelectorAll(".btn")
+const foot = document.getElementById("foot")
 
-btn.addEventListener('click', (e) => {
+btnNav.addEventListener('click', () => {
     console.log(nav)
     if(nav.classList == 'nav'){
         nav.classList = 'nav active'
+        btnIcon.style.display = 'block'
     }else{
         nav.classList = 'nav'
     }
 })
+
+btnIcon.addEventListener('click', () => {
+    console.log(btnIcon)
+    nav.classList = 'nav'
+    btnIcon.style.display = 'none'
+})
+
+function follow(){
+    window.scroll(foot.offsetLeft, foot.offsetTop)
+}
